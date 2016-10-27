2b1774b8d9957e649abdc3ad2dba2b2a410e6a06
stanimir <stanimir@stanimir-Inspiron-5559>
2016-10-27 11:32:43 +0300

    commit

--- crud_human/src/main/java/com/stanimir/controller/HumanController.java
+++ crud_human/src/main/java/com/stanimir/controller/HumanController.java
@@ -8,11 +8,9 @@ package com.stanimir.controller;
 import com.stanimir.model.Human;
 import com.stanimir.service.HumanService;
 import java.util.Collection;
-import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.MediaType;
-import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,52 +24,50 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/humans")
 public class HumanController {
-    
+
     @Autowired
     @Qualifier("embedDB")
     private HumanService humanService;
-    
+
     @RequestMapping(method = RequestMethod.GET)
-    public Collection<Human> getAllHumans()
-    {
+    public Collection<Human> getAllHumans() {
         return humanService.getAllHumans();
-        
+
     }
-    
+
     @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
-    public void insertHuman(@RequestBody Human human){
+    public void insertHuman(@RequestBody Human human) {
         humanService.insertHuman(human);
     }
-    
+
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-    public Human getHumanById(@PathVariable("id") Long id){
+    public Human getHumanById(@PathVariable("id") Long id) {
         return humanService.getHumanById(id);
     }
-    
+
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-    public void deleteHumanById(@PathVariable("id") Long id){
+    public void deleteHumanById(@PathVariable("id") Long id) {
         humanService.removeHumanById(id);
     }
-    
+
     @RequestMapping(method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE)
-    public Human updateHuman(@RequestBody Human human)
-    {
+    public Human updateHuman(@RequestBody Human human) {
         return humanService.updateHuman(human);
     }
-    
+
     @RequestMapping(value = "/sortByLastName", method = RequestMethod.GET)
-    public Collection<Human> sortByLastName(){
+    public Collection<Human> sortByLastName() {
         return humanService.sortByLastName();
     }
-    
+
     @RequestMapping(value = "/sortByDateOfBirth", method = RequestMethod.GET)
-    public Collection<Human> sortByDateOfBirth(){
+    public Collection<Human> sortByDateOfBirth() {
         return humanService.sortByDateOfBirth();
     }
-    
+
     //TODO: fix
     @RequestMapping(value = "/find/{value}", method = RequestMethod.GET)
-    public Collection<Human> find(@PathVariable String value){
+    public Collection<Human> find(@PathVariable String value) {
         return humanService.findHumansBy(value);
     }
 }
