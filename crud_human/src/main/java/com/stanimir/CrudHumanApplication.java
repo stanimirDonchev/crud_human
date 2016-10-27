14c1e33af0bc378f5d71f6b36d4e66c91cf25880
stanimir <stanimir@stanimir-Inspiron-5559>
2016-10-27 16:53:30 +0300

    commit

--- crud_human/src/main/java/com/stanimir/CrudHumanApplication.java
+++ crud_human/src/main/java/com/stanimir/CrudHumanApplication.java
@@ -4,10 +4,37 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CrudHumanApplication {
+public class CrudHumanApplication {//implements CommandLineRunner{
 
+//    @Autowired
+//    private UserRepository userRepository;
+//
+//    @Autowired
+//    private UserRolesRepository userRolesRepository;
     public static void main(String[] args) {
         SpringApplication.run(CrudHumanApplication.class, args);
     }
 
+//    @Override
+//    public void run(String... args) throws Exception {
+//        User pesho = new User();
+//        pesho.setUsername("pesho");
+//        pesho.setPassword(new BCryptPasswordEncoder().encode("pesho"));
+//        userRepository.save(pesho);
+//
+//        User gosho = new User();
+//        gosho.setUsername("gosho");
+//        gosho.setPassword(new BCryptPasswordEncoder().encode("gosho"));
+//        userRepository.save(gosho);
+//
+//        UserRoles userRolePesho = new UserRoles();
+//        userRolePesho.setUsername("pesho");
+//        userRolePesho.setRole("USER");
+//        userRolesRepository.save(userRolePesho);
+//
+//        UserRoles userRoleGosho = new UserRoles();
+//        userRoleGosho.setUsername("gosho");
+//        userRoleGosho.setRole("USER");
+//        userRolesRepository.save(userRoleGosho);
+//    }
 }
